357523fa4327cebf5032b20bfb21009edeecd202
rdos-san <raquelv.jacques@gmail.com>
2024-09-18 20:01:00 -0300

    feat: add memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,43 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rdos-san <rdos-san@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/17 17:01:19 by rdos-san          #+#    #+#             */
-/*   Updated: 2024/09/18 19:49:24 by rdos-san         ###   ########.fr       */
+/*   Created: 2024/09/18 19:52:25 by rdos-san          #+#    #+#             */
+/*   Updated: 2024/09/18 19:59:36 by rdos-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
-
-void	*memcpy(void *destination, const void *source, size_t num)
+void	*mommove(void *destination, const void * source, size_t num)
 {
-	size_t	c;
-	char	*dest;
-	char	*sou;
 
-	c = 0;
-	dest = (char *)destination;
-	sou = (char *)source;
-	while(c < num)
-	{
-		dest[c] = sou[c];
-		c++;
-	}
-	return(destination);
 }
 
 int	main(void)
 {
-	char	*str1 = "string";
-	char	str2[10];
-	size_t	n = 3;
 
-	printf("%s | %s \n", str2, str1);
-	memcpy(str2, str1, n);
-	printf("%s | %s \n", str2, str1);
 	return(0);
 }
