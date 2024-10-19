10ec7fe3a576b37f6b9cb14a66c31442fa8d297d
raquel.jacques <raquel.jacques@grupoboticario.com.br>
2024-10-19 11:22:51 -0300

    fix: import malloc funcion

--- ft_calloc.c
+++ ft_calloc.c
@@ -10,6 +10,9 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+//to use malloc is necessary to include stdlib.h because i declared the malloc library in the header file
+#include "libft.h"
+
 // this function is used to allocate memory for an array of nmemb elements of size bytes each and returns a pointer to the allocated memory. The memory is set to zero. If nmemb or size is 0, then calloc() returns either NULL, or a unique pointer value that can later be successfully passed to free().
 void	*ft_calloc(size_t nmemb, size_t size)
 {
